0a0506a4cc047e5ac9399498e72428a1b178ef5e
Jonathan Reimer-Berg <86084308+JonathanReimerBerg@users.noreply.github.com>
2025-08-04 23:14:38 -0700

    Add files via upload

--- BirdDataProject(Python, Pandas, SQL)/polkdata_main.py
+++ BirdDataProject(Python, Pandas, SQL)/polkdata_main.py
@@ -3,8 +3,13 @@ import ProjectFiles.panda_methods as pm
 import ProjectFiles.DB_writer as dw
 import os
 
+all_Possible_Locations = ['Oregon', 'Polk', 'Marion', 'Yamhill', 'Benton', 'Linn', 'Lane', 'Clackamas', 'Washington', 'Columbia', 'Multnomah',
+                    'Clatsop', 'Tillamook', 'Lincoln', 'Douglas', 'Coos', 'Curry', 'Jackson', 'Josaphine', 'Hood River', 'Wasco', 'Sherman',
+                    'Gilliam', 'Morrow', 'Umatilla', 'Union', 'Baker', 'Malheur', 'Wallawa', 'Grant', 'Wheeler', 'Deschutes', 'Klamath', 
+                    'Lake', 'Harney', 'Crook', 'Jefferson']    #this is a static list of all compatable locations this database is compatible for
 
-def inputLoc(hotspots):  #attempts to autofill a given location
+
+def selectHotspot(hotspots):  #attempts to autofill a given location
     hotspot = input("Enter hotspot name (enough chars to identify, spelling must be exact):\n")
     for spot in hotspots:
         if hotspot in spot and len(hotspot) > 4: #need to address locations with similar names
@@ -12,46 +17,70 @@ def inputLoc(hotspots):  #attempts to autofill a given location
             return(spot)
     spot= None
     if input("loc not found, press 't' to try again: ") == 't':
-        spot = inputLoc(hotspots)  #recursive function, above line insures no return if loc not found, needs to be before function call
+        spot = selectHotspot(hotspots)  #recursive function, above line insures no return if loc not found, needs to be before function call
     return(spot)
 
-def setup(Loc = None):
-    os.mkdir('ProjectFiles/DBs')
-    os.mkdir('ProjectFiles/CSVs')
-    dw.initializeDB()
-    pm.cleanCSV()
+def setupLoc(loc, new = False):
+    if new:
+        os.mkdir('ProjectFiles/DBs')
+        os.mkdir('ProjectFiles/CSVs')
+    dw.initializeDB(loc)
+    pm.cleanCSV(loc)
     return
 
+def pickLoc():
+    while True:
+        loc = input('Choose a county (or the whole state): ')
+        if loc not in all_Possible_Locations:
+            print(str(loc), "not found")
+        else:
+            break
+    return(loc)
+
 def main():
-    if not os.path.isfile('ProjectFiles/DBs/PolkDatabase.db'):
-        setup()
+    if not os.path.isfile('ProjectFiles/DBs/OregonDatabase.db'):
+        setupLoc('Oregon', True)
     while True:   #option menu, trying to keep simple functionality
         inp = input('''\nu: update data \np: print life list \ns: species summary \ny: print year list \nh: hotspot list \nc: high counts \nq: quit \n''')
         if inp == 'u':
-            dw.updateDB()
-        elif inp == 'p':
-           pm.lifelist(True)
-        elif inp == 's':
-            species = input("Species: ")
-            if species in pm.lifelist():
-                sm.speciesData(species)
-                pm.scatterplot(species)
+            doAll = input("Press '1' to update/add all Oregon regions (this may take a while): ")
+            if doAll == '1':
+                loc = all_Possible_Locations
             else:
-                print('\n', species, 'not found')
+                loc = [pickLoc()]
+            for i in loc:
+                if not os.path.isfile('ProjectFiles/DBs/' + i + 'Database.db'):
+                    setupLoc(i)
+                dw.updateDB(i)
+        elif inp == 'p':
+           pm.lifelist(pickLoc(), True)
+        elif inp == 's':
+            loc = pickLoc()
+            while True:
+                species = input("Species: ")
+                if species in pm.lifelist(loc):
+                    sm.speciesData(species, loc)
+                    pm.scatterplot(species, loc)
+                    break
+                else:
+                    print('\n', species, 'not found')
         elif inp == 'y':
+            loc = pickLoc()
             year = input("Enter a year: ")
             seen = input("Would you like to see first seen (f) or last (l): ")
-            pm.yearlist(year, seen == 'l')
+            pm.yearlist(year, loc, seen == 'l')
         elif inp == 'h':
-            hotspots = pm.hotspotList()
+            loc = pickLoc()
+            hotspots = pm.hotspotList(loc)
             for i in hotspots:
                 print(i)
-            hotspot = inputLoc(hotspots)
+            hotspot = selectHotspot(hotspots)
             if hotspot:
-                sm.hotspotList(pm.lifelist(), hotspot) 
+                sm.hotspotList(pm.lifelist(loc), hotspot, loc) 
         elif inp == 'c':
+            loc = pickLoc()
             year = input("Enter a year: ")
-            sm.highCounts(pm.lifelist(), year)
+            sm.highCounts(pm.lifelist(loc), year, loc)
         elif inp == 'q':
             break
 
